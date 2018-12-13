f7238f1c5e4f3a10b2d135165c667e9d85aecf7f
Matthew Rosenberg <mateo@Matthews-MacBook-Pro.local>
2018-12-13 12:49:46 -0700

    Completed 3 of the 4 reduce problems

--- reduce_intro_warmup/main.js
+++ reduce_intro_warmup/main.js
@@ -1,23 +1,50 @@
 
 //add all elements together
-function sum(arr){
+// function sum(arr) { 
+//   let sumAll = arr.reduce((acc, cur) => acc + cur, 0) 
+// return sumAll
+// }
 
+function sum(arr) { 
+  return arr.reduce((acc, cur) => acc + cur, 0) 
 }
 
 // multiply all elements together
 function multiply(arr){
-
+  return arr.reduce((acc, cur) => acc * cur, 1)
 }
 
 //double each element using reduce
 function double(arr){
-
+  return arr.reduce((acc, cur) => acc += cur * 2)
 }
 
 // //count the letters
-function countLetters(str){
 
+function countLetters(str){
+  let string = []
+  for (let i=0; i < str.length; i++){
+    string[i] = str[i]
+  }
+ 
+  return string.reduce((prev, cur) => {
+    // if (prev[cur] === undefined) {
+    //   prev[cur] = 1
+    // } else {
+    //   prev[cur]++
+    // }
+
+    // prev[cur] = (prev[cur] || 0) + 1
+    prev[cur] = prev[cur] === undefined ? prev[cur] = 1 : prev[cur]+1
+    
+    return prev
+  }, {})
 }
+
+countLetters(str)
+
+
+
 
 module.exports = {
   sum: sum,
